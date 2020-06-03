47fc973d7d698037e33d7e0172099c5877ff85d6
bertbezal <61188840+bertbezal@users.noreply.github.com>
2020-06-03 12:02:43 +0300

    Add files via upload

---
+++ DATAFRID/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace datagridView
+{
+    public partial class Form1 : Form
+
+    {
+        public string CmdText = "SELECT*FROM [table]";
+        public string ConnString = "provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\\Users\\BertBezal\\Desktop\\Учеба\\Гостиничный комплекс.mdb";
+        public Form1()
+        {
+            InitializeComponent();
+            OleDbDataAdapter dataAdapter = new OleDbDataAdapter(CmdText, ConnString);
+            DataSet ds = new DataSet();
+            dataAdapter.Fill(ds, "[table]");
+            dataGridView.DataSource = ds.Tables[0].DefaultView;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
